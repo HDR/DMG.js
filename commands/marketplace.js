207fcca1b716eaca0f2e9b7db05cf32c36c8ab33
HDR <20230450+HDR@users.noreply.github.com>
2021-02-03 19:06:24 +0100

    Update marketplace.js

--- commands/marketplace.js
+++ commands/marketplace.js
@@ -3,7 +3,7 @@ const { marketChannel } = require('../config.json')
 
 constants.client.on('message', async msg => {
     if (msg.channel === constants.client.channels.cache.get(marketChannel)){
-        var emojis = [':BUYING:', ':SELLING:', ':WTB:', ':WTS:', ':TRADING:', ':WTT:'];
+        let emojis = [':BUYING:', ':SELLING:', ':WTB:', ':WTS:', ':TRADING:', ':WTT:'];
         let timeSinceJoin = new Date(Math.abs(msg.member.joinedAt - Date.now()));
         let timeSinceCreated = new Date(Math.abs(msg.author.createdAt - Date.now()));
         if (timeSinceJoin/1000/60/60/24 < 30 || timeSinceCreated/1000/60/60/24 < 90) {
@@ -20,12 +20,12 @@ constants.client.on('message', async msg => {
                                 message.unpin();
                             }
                         }));
-                        msg.author.send("https://tenor.com/view/hello-there-gif-9442662");
-                        msg.author.send("Hello there, this is just a reminder that you should include the region/country in your listing and if you're selling something please include the price, this makes it easier for other users to navigate the marketplace");
                         msg.pin();
 
                     }
                 })
+                await msg.author.send("https://tenor.com/view/hello-there-gif-9442662");
+                await msg.author.send("Hello there, this is just a reminder that you should include the region/country in your listing and if you're selling something please include the price, this makes it easier for other users to navigate the marketplace");
             } else {
                 if (!(msg.author.bot || msg.member.roles.cache.has('247002823089192971') || msg.member.roles.cache.has('573611775975620608'))) {
                     await msg.delete()
