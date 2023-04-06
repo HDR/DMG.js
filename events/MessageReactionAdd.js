8828309156f64a9c10fe9ab9a7e740ef80e4a20b
Martin Refseth <20230450+HDR@users.noreply.github.com>
2023-04-07 00:34:26 +0200

    Update MessageReactionAdd.js

--- events/MessageReactionAdd.js
+++ events/MessageReactionAdd.js
@@ -10,7 +10,6 @@ client.on(Events.MessageReactionAdd, async (reaction, user) => {
         Routes.channelMessage(reaction.message.channelId, reaction.message.id)
     )
     if(data.reactions[Object.keys(data.reactions).length-1].count_details.burst >= 1) {
-        console.log(data.reactions[Object.keys(data.reactions).length-1])
         reaction.remove()
     }
 })
