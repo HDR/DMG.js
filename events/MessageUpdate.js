99f9a6290a3f3cb017a3a9291e3d017293b6cbbb
HDR <refshet@gmail.com>
2024-03-17 17:38:05 +0100

    Added MessageBulkDelete.js, Updated events.json, Updated MessageUpdate.js
    - Added MessageBulkDelete.js this will log bulk deleted messages (on ban for example) to pastebin, a pastebin api key needs to be provided in events.json
    
    - Updated MessageUpdate.js embeds should no longer be sent when nothing was changed

--- events/MessageUpdate.js
+++ events/MessageUpdate.js
@@ -4,33 +4,40 @@ const { log_channel } = require("./config/events.json")
 
 
 client.on(Events.MessageUpdate, async(oldMessage, newMessage) => {
-    if(oldMessage.author.id !== client.user.id && oldMessage.guild){
-        let Embed = new EmbedBuilder()
-        Embed.setColor('#ae3ffd')
-        Embed.setAuthor({name: `${oldMessage.author.tag}`, iconURL: `${oldMessage.author.displayAvatarURL()}`})
-        Embed.setDescription(`**${oldMessage.author.tag}** updated their message in <#${oldMessage.channel.id}>`)
-        Embed.addFields(
-            {
-                name: 'Channel',
-                value: `<#${oldMessage.channel.id}> (${oldMessage.channel.name})\n[Go to message](${oldMessage.url})`
-            },
-            {
-                name: 'New Message',
-                value: newMessage.content
-            },
-            {
-                name: 'Old Message',
-                value: oldMessage.content
-            },
-            {
-                name: 'ID',
-                value: `\`\`\`ansi\n[0;33mMember = ${oldMessage.author.id}\n[0;32mMessage = ${oldMessage.id}\`\`\``
+    if(!oldMessage.thread) {
+        if(oldMessage.guild && oldMessage.author.id !== client.user.id && oldMessage.content !== '' && oldMessage.content !== newMessage.content){
+            try {
+                let Embed = new EmbedBuilder()
+                Embed.setColor('#ae3ffd')
+                Embed.setAuthor({name: `${oldMessage.author.tag}`, iconURL: `${oldMessage.author.displayAvatarURL()}`})
+                Embed.setDescription(`**${oldMessage.author.tag}** updated their message in <#${oldMessage.channel.id}>`)
+                Embed.addFields(
+                    {
+                        name: 'Channel',
+                        value: `<#${oldMessage.channel.id}> (${oldMessage.channel.name})\n[Go to message](${oldMessage.url})`
+                    },
+                    {
+                        name: 'New Message',
+                        value: newMessage.content
+                    },
+                    {
+                        name: 'Old Message',
+                        value: oldMessage.content
+                    },
+                    {
+                        name: 'ID',
+                        value: `\`\`\`ansi\n[0;33mMember = ${oldMessage.author.id}\n[0;32mMessage = ${oldMessage.id}\`\`\``
+                    }
+                )
+
+                Embed.setTimestamp()
+                Embed.setFooter({text: `${client.user.tag}`, iconURL: `${client.user.displayAvatarURL()}`})
+
+                await oldMessage.guild.channels.cache.get(log_channel).send({embeds: [Embed]});
+            } catch (e) {
+                console.log(e)
+                console.log('Something went wrong logging the message update event, fix this later')
             }
-        )
-
-        Embed.setTimestamp()
-        Embed.setFooter({text: `${client.user.tag}`, iconURL: `${client.user.displayAvatarURL()}`})
-
-        await oldMessage.guild.channels.cache.get(log_channel).send({embeds: [Embed]});
+        }
     }
 })
