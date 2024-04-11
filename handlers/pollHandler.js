2ee3ba28298ba673474cba2e847dff764cbd2f5c
Martin Refseth <20230450+HDR@users.noreply.github.com>
2024-04-11 04:00:55 +0200

    Update pollHandler.js

--- handlers/pollHandler.js
+++ handlers/pollHandler.js
@@ -1,10 +1,10 @@
 const {client} = require("../constants");
 const {Events, PermissionsBitField} = require('discord.js');
 
-client.on(Events.Raw, async (packet) => {
-    if (packet.d.poll) {
-        let channel = await client.channels.fetch(packet.d.channel_id)
-        let message = await channel.messages.fetch(packet.d.id)
+client.on(Events.Raw, async (events) => {
+    if (events.d.poll) {
+        let channel = await client.channels.fetch(events.d.channel_id)
+        let message = await channel.messages.fetch(events.d.id)
         if(!message.member.permissions.has(PermissionsBitField.Flags.ManageGuild) && !message.author.bot) {
             await message.author.send({content: `[${message.guild.name}]: Users are not allowed to create polls in this server`}).catch(err => {
                 if(message.guild) {
