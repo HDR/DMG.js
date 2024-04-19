96266f8213f829f57981809b7c24a0046e1b32f3
HDR <refshet@gmail.com>
2024-04-19 14:10:27 +0200

    Remove pollHandler.js, Add superReactHandler.js, Update troubleshootingHandler.js
    - Removed pollHandler.js, as polls now have a separate permission
    - Added superReactHandler.js, to auto remove super reactions
    - Updated troubleshootingHandler.js to post wiki links associated with the selected post tags

--- handlers/troubleshootingHandler.js
+++ handlers/troubleshootingHandler.js
@@ -1,12 +1,27 @@
-const {client} = require("../constants");
-const {Events} = require("discord.js");
-const {troubleshooting_channel} = require("./config/troubleshootingHandler.json");
-
+const { client } = require("../constants");
+const { Events } = require("discord.js");
+const { troubleshooting_channel } = require("./config/troubleshootingHandler.json");
 
 client.on(Events.ThreadCreate, async thread => {
     if (thread.parentId === troubleshooting_channel) {
-        setTimeout(function() {
-            thread.send({content: "Please remember to provide as much information as possible including all troubleshooting steps you have already taken. If this is a hardware issue, please provide clear in-focus images of the issue, PCB (circuit board), installation, and any soldering you have done. Help us help you, posts with insufficient information will be deleted"})
+        const tagWikiMap = {
+            'Game Boy Advance': 'https://gbwiki.org/en/consoles/advance',
+            'Game Boy Advance SP': 'https://gbwiki.org/en/consoles/advancesp',
+            'Game Boy': 'https://gbwiki.org/en/consoles/gameboy',
+            'Game Boy Color': 'https://gbwiki.org/en/consoles/color',
+            'Game Boy Micro': 'https://gbwiki.org/en/consoles/micro',
+            'Game Boy Pocket': 'https://gbwiki.org/en/consoles/pocket',
+            'Game Boy Light': 'https://gbwiki.org/en/consoles/light',
+            'Game Cartridge': 'https://gbwiki.org/en/other/commonissues#game-cartridge-problems',
+        };
+
+        let tag = thread.appliedTags.map(s => thread.parent.availableTags.find(t => t.id === s)).map(x => x.name)[0];
+        let wikiString = "Please remember to provide as much information as possible including all troubleshooting steps you have already taken. If this is a hardware issue, please provide clear in-focus images of the issue, PCB (circuit board), installation, and any soldering you have done. Help us help you, posts with insufficient information will be deleted\n\n";
+
+        wikiString += tagWikiMap[tag] ? `Because this post has the "${tag}" tag, we recommend checking out this wiki page ${tagWikiMap[tag]}` : 'We also recommend checking the wiki https://gbwiki.org/';
+
+        setTimeout(function () {
+            thread.send({ content: wikiString })
         }, 10000)
     }
 });
