ffd2ca8fa2601929e453b15545a5dba3610eed8a
HDR <refshet@gmail.com>
2024-09-19 05:20:56 +0200

    Update GuildMemberAdd.js

--- events/GuildMemberAdd.js
+++ events/GuildMemberAdd.js
@@ -10,7 +10,7 @@ client.on('guildMemberAdd', async (member ) => {
 
     //Auto kick users that are too new
     let currentDate = new Date();
-    if(Math.trunc(Math.ceil(currentDate.getTime() - member.user.createdAt.getTime()) / (1000 * 3600 * 24)) < 7 && !member.user.bot) {
-        member.kick('Account is too new (less than 7 days old)')
+    if(Math.trunc(Math.ceil(currentDate.getTime() - member.user.createdAt.getTime()) / (1000 * 3600 * 24)) < 28 && !member.user.bot) {
+        member.kick('Account is too new (less than 28 days old)')
     }
 });
